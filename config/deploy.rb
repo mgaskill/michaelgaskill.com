c3c7f27f94e16f02fc9558b13ba68cb483787578
mgaskill <michaelgaskill@rocketmail.com>
2017-02-11 07:55:23 -0800

    Remove unneeded capistrano linked files and dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,7 @@ set :application, 'michaelgaskill.com'
 set :repo_url, 'https://github.com/mgaskill/michaelgaskill.com.git'
 set :branch, 'master'
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 
 
 # Default branch is :master
